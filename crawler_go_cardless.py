063ddaf8cfa7077fe0eceb902738bf01f283e67b
Sunil Kumar C <sunilkumar.c682@gmail.com>
2016-10-10 21:36:10 +0530

    New crawler added to crawl a given domain
    And print static assets on each page

--- crawler_go_cardless.py
+++ crawler_go_cardless.py
@@ -5,11 +5,8 @@ from bs4 import BeautifulSoup, SoupStrainer
 from urllib import parse
 import re
 
-robots_exclusion = ''
-
-# represents Links Repository
+# Represents Links Repository
 class LinksDatabase():
-
     def __init__(self):
         self.links = set()
 
@@ -24,38 +21,46 @@ class LinksDatabase():
             return True
         return False
 
-links_db = LinksDatabase()
-
-# reprents a page
+# Reprents a page
 class Page():
-
     def __init__(self, url):
         self.links = []
+        self.assets = []
         self.baseURL = url
+        self.resp = requests.get(self.baseURL)
+        self.page = self.resp.text
 
     def getAssets(self):
-        return ['asset']
+        try:
+            if self.resp.status_code != 200:
+                print('Something went wrong. Check the URL')
+            else:
+                # parse all the links on the page
+                assets = BeautifulSoup(self.page, 'html.parser', parse_only=SoupStrainer(['script', 'link', 'img']))
+
+                for asset in assets:
+                    if asset.has_attr('src'):
+                        self.assets.append(asset['src'])
+
+                results = assets.findAll('link', {'rel': ['icon', 'stylesheet', 'image/png', 'image/jpg', 'apple-touch-icon-precomposed']});
+                for link in results:
+                    self.assets.append(link['href'])
+
+        except requests.exceptions.MissingSchema as error:
+            print(error)
+
+        return self.assets
 
     def getBaseUrl(self):
         return self.baseURL
 
     def getLinks(self):
         try:
-            # get the source text of the page
-            resp = requests.get(self.baseURL)
-            page = resp.text
-
-            if resp.status_code != 200:
+            if self.resp.status_code != 200:
                 print('Something went wrong. Check the URL')
             else:
                 # parse all the links on the page
-                links = BeautifulSoup(page, "html.parser", parse_only=SoupStrainer('a'))
-                # assets = BeautifulSoup(page, "html.parser", parse_only=SoupStrainer(['img', 'script']))
-
-                # for asset in assets:
-                #     if asset.has_attr('src'):
-                #         print(asset['src'])
-                # hanlding relative and absolute URLs
+                links = BeautifulSoup(self.page, "html.parser", parse_only=SoupStrainer('a'))
                 for link in links:
                     if link.has_attr('href'):
                         self.links.append(parse.urljoin(self.baseURL, link['href']))
@@ -65,43 +70,47 @@ class Page():
 
         return self.links
 
+# Represents a Crawler
+class Crawler():
+    def __init__(self, startUrl):
+        self.startUrl = startUrl
+
+    def startSpider(self):
+        pagesToVisit = [self.startUrl]
+
+        while len(pagesToVisit) > 0:
+            url = pagesToVisit.pop()
+
+            if not links_db.is_visited(url):
+                links_db.add_link(url)
+                print('\nVisiting : ' + url)
+                page = Page(url)
+                linksOnThePage = page.getLinks()
+                assetsOnThePage = page.getAssets()
+
+                for asset in assetsOnThePage:
+                    print('      |___  ' + asset)
+
+                for link in linksOnThePage:
+                    if re.match(startUrl + '*', link) and (re.match(robots_exclusion, link) == None):
+                        pagesToVisit.append(link)
+
+# Handler to handle CTRL+C signal
 def handle(signal, frame):
     print('\nCrawling has been stopped!')
-    links_db.print_repository()
     sys.exit(0)
 
 def registerSignal():
     signal.signal(signal.SIGINT, handle)
 
-def startSpider(startUrl):
-    pagesToVisit = [startUrl]
-
-    while len(pagesToVisit) > 0:
-        url = pagesToVisit.pop()
-        # print('Popped : ', url)
-        if not links_db.is_visited(url):
-            links_db.add_link(url)
-            # print('Visiting : ', url)
-            page = Page(url)
-            linksOnThePage = page.getLinks()
-            assetsOnThePage = page.getAssets()
-            print(page.getBaseUrl())
-            for asset in assetsOnThePage:
-                print('      |_' + asset)
-            # print(linksOnThePage)
-            for link in linksOnThePage:
-                # print('Added : ', link)
-                if re.match(startUrl + '*', link) and (re.match(robots_exclusion, link) == None):
-                    pagesToVisit.append(link)
-
+# Function to read robots.txt and prepare regex to skip Disallowed resources
 def readRobotsTxt(url):
-    print(url)
     global robots_exclusion
     try:
         resp = requests.get(url)
         robots = resp.text
         robots = robots.split('\n')
-        # print(robots)
+
         line = 0
         for line in range(0, len(robots)):
             if 'User-agent: *' in robots[line]:
@@ -122,17 +131,15 @@ def readRobotsTxt(url):
         print(e)
         robots_exclusion = '.*'
 
-    # print('Exclusion : ' + robots_exclusion)
+# regex to consider robots.txt
+robots_exclusion = ''
+
+# Local db to store already visited links
+links_db = LinksDatabase()
 
 # main function
 if __name__=='__main__':
-    startUrl = input('Enter start URL (Default - https://gocardless.com) : ').strip()
-
-    if startUrl == '':
-        startUrl = 'https://gocardless.com'
-
-    # if not (startUrl[:10] != 'http://www' and startUrl[:11] != 'https://www'):
-    #     startUrl = 'https://www.' + startUrl
+    startUrl = input('Enter start URL (Default - https://gocardless.com) : ').strip() or 'https://gocardless.com'
 
     if startUrl[-1] != '/':
         startUrl += '/'
@@ -140,9 +147,10 @@ if __name__=='__main__':
     # register the handler to handle CTRL+C event
     registerSignal()
 
-    # Read robots.txt and build regex to skips pages later
+    # Read robots.txt and build regex to skips resources mentioned in the file.
     readRobotsTxt(startUrl + 'robots.txt')
 
-    # start the spider here
-    startSpider(startUrl)
-    
+    # Start the crawler here
+    print('Starting crawler. Press CTRL+C to stop crawling.')
+    crawler = Crawler(startUrl)
+    crawler.startSpider()
